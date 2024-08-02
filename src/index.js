1717f26360e3aa880ea40ba46702a693cbcd69f8
Milnerrafe <106588878+Milnerrafe@users.noreply.github.com>
2024-08-02 17:08:40 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ export default {
 		const path = url.pathname.split('/');
 		if (path[1] === 'api') {
 			if (request.method === 'GET') {
-				return handleGetRequest(path[2], env);
+				return handleGetRequest(env);
 			} else if (request.method === 'POST') {
 				return handlePostRequest(request, env);
 			}
@@ -13,43 +13,41 @@ export default {
 	},
 };
 
-async function handleGetRequest(playerName, env) {
-	if (playerName) {
-		const playerData = await env.HAND_BALL_PLAYER_DATA.get(playerName, 'json');
-		if (playerData) {
-			return new Response(JSON.stringify(playerData), { status: 200 });
-		}
-		return new Response('Player not found', { status: 404 });
-	} else {
-		const keys = await env.HAND_BALL_PLAYER_DATA.list();
-		let players = [];
-		for (let key of keys.keys) {
-			const playerData = await env.HAND_BALL_PLAYER_DATA.get(key.name, 'json');
-			players.push(playerData);
-		}
-		return new Response(JSON.stringify(players), { status: 200 });
+async function handleGetRequest(env) {
+	const keys = await env.HAND_BALL_PLAYER_DATA.list();
+	let players = [];
+	for (let key of keys.keys) {
+		const playerData = await env.HAND_BALL_PLAYER_DATA.get(key.name, 'json');
+		players.push(playerData);
 	}
+	return new Response(JSON.stringify(players), { status: 200 });
 }
 
 async function handlePostRequest(request, env) {
 	const data = await request.json();
-	const existingPlayer = await env.HAND_BALL_PLAYER_DATA.get(data.name, 'json');
-	if (existingPlayer) {
+	if (data.index !== undefined) {
 		// Update existing player
-		existingPlayer[data.role]++;
-		await env.HAND_BALL_PLAYER_DATA.put(data.name, JSON.stringify(existingPlayer));
+		const existingPlayer = await env.HAND_BALL_PLAYER_DATA.get(data.index, 'json');
+		if (existingPlayer) {
+			existingPlayer[data.role]++;
+			await env.HAND_BALL_PLAYER_DATA.put(data.index, JSON.stringify(existingPlayer));
+		} else {
+			return new Response('Player not found', { status: 404 });
+		}
 	} else {
 		// Add new player
+		const keys = await env.HAND_BALL_PLAYER_DATA.list();
+		const index = keys.keys.length ? Math.max(...keys.keys.map((key) => parseInt(key.name, 10))) + 1 : 1;
 		await env.HAND_BALL_PLAYER_DATA.put(
-			data.name,
+			index.toString(),
 			JSON.stringify({
+				index,
 				name: data.name,
 				img: data.img,
 				king: 0,
 				pawn: 0,
 				knight: 0,
 				queen: 0,
-				[data.role]: 1,
 			}),
 		);
 	}
