a0d58d16b1819ed1e68445728149340c9e7b78b9
Milnerrafe <106588878+Milnerrafe@users.noreply.github.com>
2024-08-02 17:26:59 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,38 @@
-var src_default = {
+export default {
 	async fetch(request, env) {
 		const url = new URL(request.url);
 		const path = url.pathname.split('/');
+
+		// CORS headers
+		const headers = new Headers();
+		headers.set('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+		headers.set('Access-Control-Allow-Methods', 'GET, POST');
+		headers.set('Access-Control-Allow-Headers', 'Content-Type');
+
+		if (request.method === 'OPTIONS') {
+			// Handle preflight requests
+			return new Response(null, {
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Methods': 'GET, POST',
+					'Access-Control-Allow-Headers': 'Content-Type',
+				},
+			});
+		}
+
 		if (path[1] === 'api') {
 			if (request.method === 'GET') {
-				return handleGetRequest(path[2], env);
+				return handleGetRequest(path[2], env, headers);
 			} else if (request.method === 'POST') {
-				return handlePostRequest(request, env);
+				return handlePostRequest(request, env, headers);
 			}
 		}
+
 		return new Response('Not Found', { status: 404 });
 	},
 };
 
-async function handleGetRequest(playerName, env) {
-	let headers = new Headers();
-	headers.set('Access-Control-Allow-Origin', '*');
-	headers.set('Access-Control-Allow-Methods', 'GET, POST');
-	headers.set('Access-Control-Allow-Headers', 'Content-Type');
-
+async function handleGetRequest(playerName, env, headers) {
 	if (playerName) {
 		const playerData = await env.HAND_BALL_PLAYER_DATA.get(playerName, 'json');
 		if (playerData) {
@@ -36,15 +50,8 @@ async function handleGetRequest(playerName, env) {
 	}
 }
 
-async function handlePostRequest(request, env) {
-	let headers = new Headers();
-	headers.set('Access-Control-Allow-Origin', '*');
-	headers.set('Access-Control-Allow-Methods', 'GET, POST');
-	headers.set('Access-Control-Allow-Headers', 'Content-Type');
-
+async function handlePostRequest(request, env, headers) {
 	const data = await request.json();
 	await env.HAND_BALL_PLAYER_DATA.put(data.index, JSON.stringify(data));
 	return new Response('Player added/updated', { status: 200, headers });
 }
-
-export { src_default as default };
